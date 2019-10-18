87bca0ccd8edf019e1a665b71a8ca696bd1cee10
muskIsai <dembossoft@gmail.com>
2019-10-18 23:26:31 +0300

    configuracao do session, flash e midlleweres

--- Balumuca/app.js
+++ Balumuca/app.js
@@ -5,8 +5,31 @@ const bodyParser = require('body-parser')
 const admin = require('./routes/admin')//buscar grupo d arquivo pra usar aqui. Abaixo vai definir apenas as tais rotas
 const path = require('path') //pra manipular as pastas
 const  expressHbs = require('express-handlebars')
+const session = require(express-session)
+const flash = require(connect-flash)
 
 const app = express()
+
+//CONFiGURACOES
+//OBS: app.use - serve p/ criar e configurar os Midlleweres/ Tdo q tem app.use eh nada+ ou nada- um Midllewere
+
+//Sessoes
+app.use(session({
+    secret: 'cursoNode_balumuca',
+    resave: true,
+    saveUnitialized: true
+
+}))
+app.use(flash()) //O flash tem q xtar sempre abaixo do "session"
+
+//Midllewre
+app.use((req, res, next) =>{
+    //vamos criar 2 variaveis globais q podem ser acessados em qlqr parte desta pagina (app.js). Eis as variaveis criadas:
+    //sucess_msm, error_msg. Sao ceriadas atraves da propriedade "locals"
+    res.locals.sucess_msg = req.flash('sucess_msg')
+    res.locals.error_msg = req.flash('error_msg')
+    next() //Vai permitir entrar ou passar (vai avancar)
+})
 
 //BodyParser
 app.use(bodyParser.urlencoded({extended: true}))
@@ -16,8 +39,6 @@ app.use(bodyParser.json())
 //app.engine(' handlebars', handlebars({defaultLayout: 'main'}))
 app.engine('.hbs', expressHbs({defaultLayout: 'main', extname: '.hbs'}))
 app.set('view engine', 'hbs')
-
-//Punlic
 
 
 //Mongoose
@@ -34,6 +55,14 @@ mongoose.connect('mongodb://localhost/balumuca_db', {
 //Publc
 app.use(express.static(path.join(__dirname, 'public')))
 //app.set('views', path.join(__dirname, 'views'))
+
+//Usando e aprendendo <<MIDLEWARE>> (so ativado assim q acontecer uma requizcao)
+/* app.use((req, res, next) => {
+    console.log('Eu sou o MidleWare'); //A App va parar aqui, va carregar mas n vai abrir
+    next(); //Vai permitir entrar ou passar (vai avancar) */
+
+    //Cada vaez q eh carregada qlqr pagina o MIDLEWARE eh chamado
+})
 
 
 //Rotas
